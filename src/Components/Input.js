b361ce2cb2aaf7191a772113c47763813174fd23
Mahtv Bansal <mahtvbansal@gmail.com>
2021-11-17 16:26:13 +0530

    code cleaned part 1

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-// import Spinner from "./Spinner";
-// import { combinations, result } from "./Function.js";
 import LoadingBar from 'react-top-loading-bar'
 
 const Input = () => {
@@ -16,7 +14,6 @@ const Input = () => {
     const [subCategory_Array, setSubCategory_Array] = useState([]);
     const [subCategory_Input, setSubCategory_Input] = useState([]);
     const [progress, setProgress] = useState(0)
-    // const [loading, setLoading] = useState(false);
 
     const rentSum = (cities) => {
       let sum = 0;
@@ -111,62 +108,12 @@ const Input = () => {
 
     const combinations = (elements , k, max_Rent, temp_Result) => {
 
-      // setLoading(true);
-
       const keepGoing = (elem) => {
         if(elem.length === k){
             return false;
         }
         return true;
       }
-    // const combinations = (elements , elements_length, index, temp_Result, combWithDetail, start) => {
-        
-
-        // if(index === Number(numOfCities)){
-
-        //   console.log("combWithDetail");
-
-        //   let temp = {Places : [], Total_Rent: 0};
-
-        //   if(rentSum(combWithDetail) <= Number(maxRent)){
-        //     if(Number(numOfCat) + Number(numOfSubCat) === 0){
-        //         temp.Places = cityNames(combWithDetail);
-        //         temp.Total_Rent = rentSum(combWithDetail);
-        //         temp_Result.push(temp);
-        //     }
-        //     else if(Number(numOfSubCat) === 0 && handleCat(combWithDetail)){
-        //         temp.Places = cityNames(combWithDetail);
-        //         temp.Total_Rent = rentSum(combWithDetail);
-        //         temp_Result.push(temp);
-        //     }
-        //     else if(Number(numOfCat) === 0 && handleSubCat(combWithDetail)){
-        //         temp.Places = cityNames(combWithDetail);
-        //         temp.Total_Rent = rentSum(combWithDetail);
-        //         temp_Result.push(temp);
-        //     }
-        //     else if(handleSubCat(combWithDetail) && handleCat(combWithDetail)){
-        //         temp.Places = cityNames(combWithDetail);
-        //         temp.Total_Rent = rentSum(combWithDetail);
-        //         temp_Result.push(temp);
-        //     }
-        //   }
-        //   return;
-        // }
-
-        // if(i >= elements_length){
-        //     return;
-        // }
-        // for(let i = start; i<= elements_length - 1 && ((elements_length - 1)-i+1 >= Number(numOfCities) - index); i++ ){
-        //     combWithDetail[index] = elements[i];
-        //     combinations(elements , elements_length, index+1, temp_Result, combWithDetail, i+1);
-        // }
-        
-        // combWithDetail[index] = elements[i];
-        // combinations(elements , elements_length, index+1, temp_Result, i+1, combWithDetail);
-
-        // combinations(elements , elements_length, index, temp_Result, i+1, combWithDetail);
-
-        // setResult(temp_Result);
 
 
         if(elements.length === 0)
@@ -179,13 +126,6 @@ const Input = () => {
         const combsWithFirst = [];
         
         combsWithoutFirst.forEach(comb =>{
-
-            // let count = 0;
-            // if(count <= 1000){
-            //   alert("Your result has more than 1000 combinations & first 1000 combinations are shown");
-            //   setResult(temp_Result);
-            //   return ;
-            // }
     
             let temp = {Places : [], Total_Rent: 0};
     
@@ -244,17 +184,14 @@ const Input = () => {
     const showResult = () => {
       setProgress(progress +10);
       setResult([]);
-      // setLoading(false);
       if (data && numOfCities && maxRent) {
           
           
           let tempResult = [];
           setProgress(progress +25);
-          // let combWithDetail = new Array(Number(numOfCities));
           const table = JSON.parse(data);
           console.log(table.length);
           setProgress(progress +40);
-          // combinations(table, table.length, 0, tempResult, combWithDetail, 0);
           let comb_Array = combinations(table, Number(numOfCities), Number(maxRent), tempResult);
           setProgress(progress +70);
           if(comb_Array.length === 0){
@@ -265,8 +202,6 @@ const Input = () => {
         alert("Please fill the data properly");
       }
       setProgress(100);
-      // setLoading(false);
-      // console.log(result);
     };
 
   const makeCategoryInputArray = (k) => {
@@ -275,7 +210,6 @@ const Input = () => {
     for(let i=0; i<k; i++) {
         let categoryObject = {id: i, cat_name: "", min_cities: '', max_cities: ''};
         let categoryInputObject = {id: i};
-        // categoryArray.push(categoryObject);
         categoryInputArray.push(categoryInputObject);
         tempCategoryArray.push(categoryObject);
     }
@@ -293,7 +227,6 @@ const Input = () => {
     for(let i=0; i<k; i++) {
       let SubcategoryObject = {id: i, subCat_name: "", min_cities: '', max_cities: ''};
       let SubcategoryInputObject = {id: i};
-      // categoryArray.push(categoryObject);
       SubcategoryInputArray.push(SubcategoryInputObject);
       tempSubCategoryArray.push(SubcategoryObject);
     }
@@ -371,7 +304,6 @@ const Input = () => {
                     <tr>
                     <th scope="col">Category Name</th>
                     <th scope="col">Minimum</th>
-                    {/* <th scope="col">Last</th> */}
                     <th scope="col">Maximum</th>
                     </tr>
                 </thead>
@@ -386,7 +318,6 @@ const Input = () => {
                                         let tempCategoryInput = [...category_Array];
                                         tempCategoryInput[elem.id].cat_name = e.target.value;
                                         setCategory_Array(tempCategoryInput);
-                                        // setCategory_Array([...category_Array, {...category_Array[elem.id] , cat_name: e.target.value}]);
                                     }} 
                                     placeholder="Name" /></th>
 
@@ -396,7 +327,6 @@ const Input = () => {
                                         let tempCategory_Input = [...category_Array];
                                         tempCategory_Input[elem.id].min_cities = e.target.value;
                                         setCategory_Array(tempCategory_Input);
-                                        // setCategory_Array([...category_Array, {...category_Array[elem.id] , min_cities: e.target.value}]);
                                     }} 
                                     placeholder="Min" /></td>
 
@@ -406,7 +336,6 @@ const Input = () => {
                                         let temp_Category_Input = [...category_Array];
                                         temp_Category_Input[elem.id].max_cities = e.target.value;
                                         setCategory_Array(temp_Category_Input);
-                                        // setCategory_Array([...category_Array, {...category_Array[elem.id] , max_cities: e.target.value}]);
                                     }} 
                                     placeholder="Max" /></td> 
                                 </tr>
@@ -432,7 +361,6 @@ const Input = () => {
                     <tr>
                     <th scope="col">Sub-category Name</th>
                     <th scope="col">Minimum</th>
-                    {/* <th scope="col">Last</th> */}
                     <th scope="col">Maximum</th>
                     </tr>
                 </thead>
